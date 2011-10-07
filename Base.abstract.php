b24152543871e748620562933c7702c0dc322a9c
Enrique Bondoc <enrique.p.bondoc@gmail.com>
2011-10-07 18:58:46 +0800

    Added documentation on Base.abstract.php

--- Base.abstract.php
+++ Base.abstract.php
@@ -2,6 +2,12 @@
 	/**
 	 *	Jellyfish_Base
 	 *
+	 *	@package	Jellyfish
+	 *	@author		Eric Bondoc <enrique.p.bondoc@gmail.com>
+	 *	@since		07-Oct-2011 06:53 PM
+	 *	@abstract
+	 *
+	 *	This serves as the base class for all supporting/extending subclasses following the Jellyfish architecture.
 	 **/
 	abstract class Jellyfish_Base {
 		
